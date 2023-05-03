8de5f597cb68f12857ec7d54c9bffbde22e3037e
tiago.landim <tiago.landim@meta.com.br>
2023-05-02 21:27:30 -0300

    ajuste status bar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import StackRoutes from "./stack.routes";
 import TabRoutes from "./tab.routes";
 import DrawerRoutes from "./drawer.routes";
+import { StatusBar } from "expo-status-bar";
 
 const Routes: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar style="light" />
       {/* <StackRoutes /> */}
       <TabRoutes />
       {/* <DrawerRoutes /> */}
